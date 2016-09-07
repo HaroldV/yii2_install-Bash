c52c7531632ee200574cdfcee20e5132ef387072
Harold Villalobos <haroldestebanvillalobos@gmail.com>
2016-09-07 11:06:50 -0400

    Update yii2_install.sh

--- yii2_install.sh
+++ yii2_install.sh
@@ -3,7 +3,9 @@
 # sh scriptname $1=(basic or advanced)  $2=projectName
 # Lic. Harold Villalobos 17/09/2015 10:49 am
 
-composer global require "fxp/composer-asset-plugin:~1.2.0"
-composer create-project --prefer-dist yiisoft/yii2-app-$1 $2
+composer global require "fxp/composer-asset-plugin:^1.2.0"
+composer create-project --prefer-dist --stability=dev yiisoft/yii2-app-$1 $2
+#composer create-project --prefer-dist yiisoft/yii2-app-$1 $2
+#composer require yiisoft/yii2
 
 
